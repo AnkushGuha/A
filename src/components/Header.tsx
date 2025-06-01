08117e09ef9ed9d75529818a10ab2cce5046d3e8
Kikuu01 <rohitkumawat03506@gmail.com>
2025-06-01 19:05:37 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,7 +14,7 @@ const Header: React.FC = () => {
    <img
   src={logo}
   alt="Logo"
-  className="h-40 sm:h-30 md:h-30 lg:h-30 w-100 object-contain"
+  className=""
 />
         {/* Desktop Nav */}
         <nav className="hidden md:flex space-x-8 items-center">
